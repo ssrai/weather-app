bc2558a784b374bd6d1eff25d5c58452cbe66280
Sudhanshu Rai <sudhanshurai97@gmail.com>
2021-11-15 13:34:39 +0530

    Status modified for App.js and extra image file removed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require("./utils/forecast");
 
 const app = express();
 
-// Define path for Express config
+// Define path for Express configurations
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
